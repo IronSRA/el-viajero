f30c5337d3139b3bc14848ba97b867427324adfd
Sergio Collado <sergiocollado95@gmail.com>
2020-02-16 23:14:30 +0100

    Changes in app to use sass and incomplete news

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express()
 
 // DB, middlewares, locals & debug
 require('./configs/mongoose.config')
-require('./configs/middleware.config')(app)
 require('./configs/preprocessor.config')(app)
+require('./configs/middleware.config')(app)
 require('./configs/locals.config')(app)
 require('./configs/debug.config')
 require('./configs/passport.config')(app)
