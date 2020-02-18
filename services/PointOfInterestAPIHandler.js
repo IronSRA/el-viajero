ab5c5b80ffca0333c1f03aed0a52c7ef46562f38
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-02-18 20:39:41 +0100

    photo

--- services/PointOfInterestAPIHandler.js
+++ services/PointOfInterestAPIHandler.js
@@ -22,26 +22,6 @@ class pointOfInterestAPIHandler {
 
   getPointsOfInterest(city, country) {
     return this.axiosApp.get(`place/textsearch/json?query=${city}+${country}+point+of+interest&key=${this.apiKey}`)
-
-
-      // https://maps.googleapis.com/maps/api/place/textsearch/json?query=new+york+city+point+of+interest&language=en&key=API_KEY
-      // .then(pointOfinterest => {
-      //   let photo = []
-      //   let info = pointOfinterest.data.results
-      //   let pointOfinterests = {
-      //     photo,
-      //     info
-      //   }
-      //   pointOfinterest.data.results.forEach(elm => {
-      //     let reference = elm.photos[0].photo_reference
-      //     return this.axiosApp.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${this.apiKey}`)
-      //       .then(res => {
-      //         photo.push(res.data)
-      //         return pointOfinterests
-      //       })
-
-      //   })
-      //})
       .catch(error => console.log('Oh No! Error is: ', error))
   }
 
